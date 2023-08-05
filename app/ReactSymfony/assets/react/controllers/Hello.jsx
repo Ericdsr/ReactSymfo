e4d5a9d003c26b4faffac7eab1adbc61d6f9abbd
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-08-05 11:35:03 +0200

    📦 NEW:

--- app/ReactSymfony/assets/react/controllers/Hello.jsx
+++ app/ReactSymfony/assets/react/controllers/Hello.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default function (props) {
-    return <div>Hello {props.fullName}</div>;
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
+
+export default ColorSchemesExample;
